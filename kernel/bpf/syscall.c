7284525a8e96b0dec97b8f0adca0535d4ee69bee
Alistair Popple <alistair@popple.id.au>
2018-07-30 14:50:58 +1000

    Revert "bpf: fix possible spectre-v1 in find_and_alloc_map()"
    This reverts commit 9ef09e35e521bf0df5325cc9cffa726a8f5f3c1b.

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -31,7 +31,6 @@
 #include <linux/timekeeping.h>
 #include <linux/ctype.h>
 #include <linux/btf.h>
-#include <linux/nospec.h>
 
 #define IS_FD_ARRAY(map) ((map)->map_type == BPF_MAP_TYPE_PROG_ARRAY || \
 			   (map)->map_type == BPF_MAP_TYPE_PERF_EVENT_ARRAY || \
@@ -108,14 +107,12 @@ const struct bpf_map_ops bpf_map_offload_ops = {
 static struct bpf_map *find_and_alloc_map(union bpf_attr *attr)
 {
 	const struct bpf_map_ops *ops;
-	u32 type = attr->map_type;
 	struct bpf_map *map;
 	int err;
 
-	if (type >= ARRAY_SIZE(bpf_map_types))
+	if (attr->map_type >= ARRAY_SIZE(bpf_map_types))
 		return ERR_PTR(-EINVAL);
-	type = array_index_nospec(type, ARRAY_SIZE(bpf_map_types));
-	ops = bpf_map_types[type];
+	ops = bpf_map_types[attr->map_type];
 	if (!ops)
 		return ERR_PTR(-EINVAL);
 
@@ -130,7 +127,7 @@ static struct bpf_map *find_and_alloc_map(union bpf_attr *attr)
 	if (IS_ERR(map))
 		return map;
 	map->ops = ops;
-	map->map_type = type;
+	map->map_type = attr->map_type;
 	return map;
 }
 
