8b3116c00f88399bee4a4dd80019537592b8a528
Alistair Popple <alistair@popple.id.au>
2018-07-30 13:21:40 +1000

    Revert "KVM: arm/arm64: vgic: fix possible spectre-v1 in vgic_get_irq()"
    This reverts commit 41b87599c74300027f305d7b34368ec558978ff2.

--- virt/kvm/arm/vgic/vgic.c
+++ virt/kvm/arm/vgic/vgic.c
@@ -14,13 +14,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <linux/interrupt.h>
-#include <linux/irq.h>
 #include <linux/kvm.h>
 #include <linux/kvm_host.h>
 #include <linux/list_sort.h>
-#include <linux/nospec.h>
-
+#include <linux/interrupt.h>
+#include <linux/irq.h>
 #include <asm/kvm_hyp.h>
 
 #include "vgic.h"
@@ -108,16 +106,12 @@ struct vgic_irq *vgic_get_irq(struct kvm *kvm, struct kvm_vcpu *vcpu,
 			      u32 intid)
 {
 	/* SGIs and PPIs */
-	if (intid <= VGIC_MAX_PRIVATE) {
-		intid = array_index_nospec(intid, VGIC_MAX_PRIVATE);
+	if (intid <= VGIC_MAX_PRIVATE)
 		return &vcpu->arch.vgic_cpu.private_irqs[intid];
-	}
 
 	/* SPIs */
-	if (intid <= VGIC_MAX_SPI) {
-		intid = array_index_nospec(intid, VGIC_MAX_SPI);
+	if (intid <= VGIC_MAX_SPI)
 		return &kvm->arch.vgic.spis[intid - VGIC_NR_PRIVATE_IRQS];
-	}
 
 	/* LPIs */
 	if (intid >= VGIC_MIN_LPI)
