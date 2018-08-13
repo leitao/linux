0494e7ef520a2cf135b0f3ff558fd41c03524459
Breno Leitao <leitao@debian.org>
2018-08-13 16:09:56 -0400

    powerpc/tm: Wire up TM_KERNEL_ENTRY
    Wire up the macro to most of the kernel entries, except:
    
      - the facility unavailable path (vsx, fp, vec)_unavailable_tm, which might be
        done later. For these cases, the treclaim will happen at the *_unavailable_tm level.
    
      - syscall. The syscall path, if invoked inside a transaction, is not
        reclaiming and recheckpointing, but aborting and returning.
    
    PS: Not clear if all these places are enough.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>

--- arch/powerpc/include/asm/exception-64s.h
+++ arch/powerpc/include/asm/exception-64s.h
@@ -726,6 +726,7 @@ END_FTR_SECTION_IFSET(CPU_FTR_CTRL)
 	EXCEPTION_PROLOG_COMMON(trap, area);			\
 	/* Volatile regs are potentially clobbered here */	\
 	additions;						\
+	TM_KERNEL_ENTRY;					\
 	addi	r3,r1,STACK_FRAME_OVERHEAD;			\
 	bl	hdlr;						\
 	b	ret
@@ -740,6 +741,7 @@ END_FTR_SECTION_IFSET(CPU_FTR_CTRL)
 	EXCEPTION_PROLOG_COMMON_3(trap);			\
 	/* Volatile regs are potentially clobbered here */	\
 	additions;						\
+	TM_KERNEL_ENTRY;					\
 	addi	r3,r1,STACK_FRAME_OVERHEAD;			\
 	bl	hdlr
 
