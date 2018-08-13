b25f9d420d7901301d1c7d60f842e61bd18ca51a
Breno Leitao <leitao@debian.org>
2018-08-13 16:54:53 -0400

    powerpc/tm: Nothing to be done on context switch
    Now that treclaim happens on kernel entrance and trecheckpoint is called on
    kernel exit, the context switch does not have to care about TM contexts.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -1031,19 +1031,8 @@ static inline void tm_recheckpoint_new_task(struct task_struct *new)
 static inline void __switch_to_tm(struct task_struct *prev,
 		struct task_struct *new)
 {
-	if (cpu_has_feature(CPU_FTR_TM)) {
-		if (tm_enabled(prev) || tm_enabled(new))
-			tm_enable();
-
-		if (tm_enabled(prev)) {
-			prev->thread.load_tm++;
-			tm_reclaim_task(prev);
-			if (!MSR_TM_ACTIVE(prev->thread.regs->msr) && prev->thread.load_tm == 0)
-				prev->thread.regs->msr &= ~MSR_TM;
-		}
-
-		tm_recheckpoint_new_task(new);
-	}
+	/* we do not need to do any TM on context switch anymore */
+	WARN_ON(MSR_TM_ACTIVE(mfmsr()));
 }
 
 /*
