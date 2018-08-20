03fec8d4b47eefe5fabc71c9dfeb7525919bfcc0
Breno Leitao <leitao@debian.org>
2018-08-20 13:54:26 -0400

    powerpc/tm: No need for tm code on task dup
    Since the task is never in suspended stated during a duplication/fork,
    there is no need to call __switch_to_tm(), which would reclaim the previous
    thread checkpointed values anymore.o This was already done on the kernel
    entrance.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -1525,17 +1525,7 @@ release_thread(struct task_struct *t)
 int arch_dup_task_struct(struct task_struct *dst, struct task_struct *src)
 {
 	flush_all_to_thread(src);
-	/*
-	 * Flush TM state out so we can copy it.  __switch_to_tm() does this
-	 * flush but it removes the checkpointed state from the current CPU and
-	 * transitions the CPU out of TM mode.  Hence we need to call
-	 * tm_recheckpoint_new_task() (on the same task) to restore the
-	 * checkpointed state back and the TM mode.
-	 *
-	 * Can't pass dst because it isn't ready. Doesn't matter, passing
-	 * dst is only important for __switch_to()
-	 */
-	__switch_to_tm(src, src);
+	WARN_ON(MSR_TM_SUSPENDED(mfmsr()));
 
 	*dst = *src;
 
