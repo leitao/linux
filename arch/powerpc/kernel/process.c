edc5e2a7f52af9ca899536be4a5a1c11ce4699ac
Breno Leitao <leitao@debian.org>
2018-10-30 17:27:21 -0400

    powerpc/tm: Restore facility after recheckpoint
    After the facility registers are recheckpointed, they are clobbered with
    the values that were recheckpointed (and are now in the checkpoint area).
    
    If these registers will have the facility enabled on returning to
    userspace, they need to be restored, otherwise userspace will see invalid
    values.
    
    This patch simplify the restore_tm_state() to just restore what is enabled
    on the way to userspace.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>

--- arch/powerpc/kernel/process.c
+++ arch/powerpc/kernel/process.c
@@ -1064,8 +1064,6 @@ static inline void switch_to_tm(struct task_struct *prev,
  */
 void restore_tm_state(struct pt_regs *regs)
 {
-	unsigned long msr_diff;
-
 	/*
 	 * This is the only moment we should clear TIF_RESTORE_TM as
 	 * it is here that ckpt_regs.msr and pt_regs.msr become the same
@@ -1081,19 +1079,12 @@ void restore_tm_state(struct pt_regs *regs)
 	/* The only place we recheckpoint */
 	tm_recheckpoint(&current->thread);
 
-	msr_diff = current->thread.ckpt_regs.msr & ~regs->msr;
-	msr_diff &= MSR_FP | MSR_VEC | MSR_VSX;
-
-	/* Ensure that restore_math() will restore */
-	if (msr_diff & MSR_FP)
-		current->thread.load_fp = 1;
-#ifdef CONFIG_ALTIVEC
-	if (cpu_has_feature(CPU_FTR_ALTIVEC) && msr_diff & MSR_VEC)
-		current->thread.load_vec = 1;
-#endif
-	restore_math(regs);
-
-	regs->msr |= msr_diff;
+	if (regs->msr & MSR_FP)
+		restore_fp(current);
+	if (regs->msr & MSR_VEC)
+		restore_altivec(current);
+	if (regs->msr & MSR_VSX)
+		restore_vsx(current);
 }
 
 #else
