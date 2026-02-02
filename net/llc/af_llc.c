f03a530f57aaa66567430567099097a497aced9f
Breno Leitao <leitao@debian.org>
2026-02-02 07:57:28 -0800

    llc: convert to getsockopt_iter
    Convert LLC's getsockopt implementation to use the new getsockopt_iter
    callback with sockopt_t.
    
    Key changes:
    - Replace (char __user *optval, int __user *optlen) with sockopt_t *opt
    - Use opt->optlen for buffer length (input) and returned size (output)
    - Use copy_to_iter() instead of put_user()/copy_to_user()
    
    This enables io_uring to call LLC's getsockopt with kernel buffers.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>

--- net/llc/af_llc.c
+++ net/llc/af_llc.c
@@ -27,6 +27,7 @@
 #include <linux/init.h>
 #include <linux/slab.h>
 #include <linux/sched/signal.h>
+#include <linux/uio.h>
 
 #include <net/llc.h>
 #include <net/llc_sap.h>
@@ -1164,24 +1165,21 @@ out:
  *	@sock: Socket to get information from.
  *	@level: Socket level user is requesting operations on.
  *	@optname: Operation name.
- *	@optval: Variable to return operation data in.
- *	@optlen: Length of optval.
+ *	@opt: Socket option iterator for returning data.
  *
  *	Get connection specific socket information.
  */
 static int llc_ui_getsockopt(struct socket *sock, int level, int optname,
-			     char __user *optval, int __user *optlen)
+			     sockopt_t *opt)
 {
 	struct sock *sk = sock->sk;
 	struct llc_sock *llc = llc_sk(sk);
-	int val = 0, len = 0, rc = -EINVAL;
+	int val = 0, len, rc = -EINVAL;
 
 	lock_sock(sk);
 	if (unlikely(level != SOL_LLC))
 		goto out;
-	rc = get_user(len, optlen);
-	if (rc)
-		goto out;
+	len = opt->optlen;
 	rc = -EINVAL;
 	if (len != sizeof(int))
 		goto out;
@@ -1209,9 +1207,12 @@ static int llc_ui_getsockopt(struct socket *sock, int level, int optname,
 		rc = -ENOPROTOOPT;
 		goto out;
 	}
-	rc = 0;
-	if (put_user(len, optlen) || copy_to_user(optval, &val, len))
+	if (copy_to_iter(&val, len, &opt->iter) != len) {
 		rc = -EFAULT;
+		goto out;
+	}
+	opt->optlen = len;
+	rc = 0;
 out:
 	release_sock(sk);
 	return rc;
@@ -1237,7 +1238,7 @@ static const struct proto_ops llc_ui_ops = {
 	.listen      = llc_ui_listen,
 	.shutdown    = llc_ui_shutdown,
 	.setsockopt  = llc_ui_setsockopt,
-	.getsockopt  = llc_ui_getsockopt,
+	.getsockopt_iter = llc_ui_getsockopt,
 	.sendmsg     = llc_ui_sendmsg,
 	.recvmsg     = llc_ui_recvmsg,
 	.mmap	     = sock_no_mmap,
