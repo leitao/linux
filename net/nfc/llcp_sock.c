91580033f57153cc819225df40fb01130a5ab82d
Breno Leitao <leitao@debian.org>
2026-02-02 07:54:45 -0800

    nfc: llcp: convert to getsockopt_iter
    Convert NFC LLCP's getsockopt implementation to use the new
    getsockopt_iter callback with sockopt_t.
    
    Key changes:
    - Replace (char __user *optval, int __user *optlen) with sockopt_t *opt
    - Use opt->optlen for buffer length (input) and returned size (output)
    - Use copy_to_iter() instead of put_user()
    
    This enables io_uring to call NFC LLCP's getsockopt with kernel buffers.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>

--- net/nfc/llcp_sock.c
+++ net/nfc/llcp_sock.c
@@ -10,6 +10,7 @@
 #include <linux/module.h>
 #include <linux/nfc.h>
 #include <linux/sched/signal.h>
+#include <linux/uio.h>
 
 #include "nfc.h"
 #include "llcp.h"
@@ -302,22 +303,20 @@ static int nfc_llcp_setsockopt(struct socket *sock, int level, int optname,
 }
 
 static int nfc_llcp_getsockopt(struct socket *sock, int level, int optname,
-			       char __user *optval, int __user *optlen)
+			       sockopt_t *opt)
 {
 	struct nfc_llcp_local *local;
 	struct sock *sk = sock->sk;
 	struct nfc_llcp_sock *llcp_sock = nfc_llcp_sock(sk);
 	int len, err = 0;
-	u16 miux, remote_miu;
-	u8 rw;
+	u32 val;
 
 	pr_debug("%p optname %d\n", sk, optname);
 
 	if (level != SOL_NFC)
 		return -ENOPROTOOPT;
 
-	if (get_user(len, optlen))
-		return -EFAULT;
+	len = opt->optlen;
 
 	local = llcp_sock->local;
 	if (!local)
@@ -329,40 +328,25 @@ static int nfc_llcp_getsockopt(struct socket *sock, int level, int optname,
 
 	switch (optname) {
 	case NFC_LLCP_RW:
-		rw = llcp_sock->rw > LLCP_MAX_RW ? local->rw : llcp_sock->rw;
-		if (put_user(rw, (u32 __user *) optval))
-			err = -EFAULT;
-
+		val = llcp_sock->rw > LLCP_MAX_RW ? local->rw : llcp_sock->rw;
 		break;
 
 	case NFC_LLCP_MIUX:
-		miux = be16_to_cpu(llcp_sock->miux) > LLCP_MAX_MIUX ?
+		val = be16_to_cpu(llcp_sock->miux) > LLCP_MAX_MIUX ?
 			be16_to_cpu(local->miux) : be16_to_cpu(llcp_sock->miux);
-
-		if (put_user(miux, (u32 __user *) optval))
-			err = -EFAULT;
-
 		break;
 
 	case NFC_LLCP_REMOTE_MIU:
-		remote_miu = llcp_sock->remote_miu > LLCP_MAX_MIU ?
+		val = llcp_sock->remote_miu > LLCP_MAX_MIU ?
 				local->remote_miu : llcp_sock->remote_miu;
-
-		if (put_user(remote_miu, (u32 __user *) optval))
-			err = -EFAULT;
-
 		break;
 
 	case NFC_LLCP_REMOTE_LTO:
-		if (put_user(local->remote_lto / 10, (u32 __user *) optval))
-			err = -EFAULT;
-
+		val = local->remote_lto / 10;
 		break;
 
 	case NFC_LLCP_REMOTE_RW:
-		if (put_user(llcp_sock->remote_rw, (u32 __user *) optval))
-			err = -EFAULT;
-
+		val = llcp_sock->remote_rw;
 		break;
 
 	default:
@@ -372,10 +356,15 @@ static int nfc_llcp_getsockopt(struct socket *sock, int level, int optname,
 
 	release_sock(sk);
 
-	if (put_user(len, optlen))
+	if (err)
+		return err;
+
+	if (copy_to_iter(&val, len, &opt->iter) != len)
 		return -EFAULT;
 
-	return err;
+	opt->optlen = len;
+
+	return 0;
 }
 
 void nfc_llcp_accept_unlink(struct sock *sk)
@@ -928,7 +917,7 @@ static const struct proto_ops llcp_sock_ops = {
 	.listen         = llcp_sock_listen,
 	.shutdown       = sock_no_shutdown,
 	.setsockopt     = nfc_llcp_setsockopt,
-	.getsockopt     = nfc_llcp_getsockopt,
+	.getsockopt_iter = nfc_llcp_getsockopt,
 	.sendmsg        = llcp_sock_sendmsg,
 	.recvmsg        = llcp_sock_recvmsg,
 	.mmap           = sock_no_mmap,
