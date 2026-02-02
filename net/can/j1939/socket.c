4b83d48d2a8a87adc87a130bfa443bb662624b25
Breno Leitao <leitao@debian.org>
2026-02-02 08:02:16 -0800

    can: j1939: convert to getsockopt_iter
    Convert CAN J1939's getsockopt implementation to use the new
    getsockopt_iter callback with sockopt_t.
    
    Key changes:
    - Replace (char __user *optval, int __user *optlen) with sockopt_t *opt
    - Use opt->optlen for buffer length (input) and returned size (output)
    - Use copy_to_iter() instead of put_user()/copy_to_user()
    
    This enables io_uring to call CAN J1939's getsockopt with kernel buffers.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>

--- net/can/j1939/socket.c
+++ net/can/j1939/socket.c
@@ -17,6 +17,7 @@
 #include <linux/can/skb.h>
 #include <linux/errqueue.h>
 #include <linux/if_arp.h>
+#include <linux/uio.h>
 
 #include "j1939-priv.h"
 
@@ -765,7 +766,7 @@ static int j1939_sk_setsockopt(struct socket *sock, int level, int optname,
 }
 
 static int j1939_sk_getsockopt(struct socket *sock, int level, int optname,
-			       char __user *optval, int __user *optlen)
+			       sockopt_t *opt)
 {
 	struct sock *sk = sock->sk;
 	struct j1939_sock *jsk = j1939_sk(sk);
@@ -777,8 +778,7 @@ static int j1939_sk_getsockopt(struct socket *sock, int level, int optname,
 
 	if (level != SOL_CAN_J1939)
 		return -EINVAL;
-	if (get_user(ulen, optlen))
-		return -EFAULT;
+	ulen = opt->optlen;
 	if (ulen < 0)
 		return -EINVAL;
 
@@ -802,14 +802,14 @@ static int j1939_sk_getsockopt(struct socket *sock, int level, int optname,
 	 * but most sockopt's are 'int' properties, and have 'len' & 'val'
 	 * left unchanged, but instead modified 'tmp'
 	 */
-	if (len > ulen)
+	if (len > ulen) {
 		ret = -EFAULT;
-	else if (put_user(len, optlen))
+	} else if (copy_to_iter(val, len, &opt->iter) != len) {
 		ret = -EFAULT;
-	else if (copy_to_user(optval, val, len))
-		ret = -EFAULT;
-	else
+	} else {
+		opt->optlen = len;
 		ret = 0;
+	}
  no_copy:
 	release_sock(&jsk->sk);
 	return ret;
@@ -1378,7 +1378,7 @@ static const struct proto_ops j1939_ops = {
 	.listen = sock_no_listen,
 	.shutdown = sock_no_shutdown,
 	.setsockopt = j1939_sk_setsockopt,
-	.getsockopt = j1939_sk_getsockopt,
+	.getsockopt_iter = j1939_sk_getsockopt,
 	.sendmsg = j1939_sk_sendmsg,
 	.recvmsg = j1939_sk_recvmsg,
 	.mmap = sock_no_mmap,
